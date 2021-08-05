2b4a193bd9613c8e26bf377653551643e439a2d4
gdilucca01 <geoffrey.dilucca@gmail.com>
2021-08-05 14:54:34 +0200

    Projet pour la gestion des utilisateurs

---
+++ src/Utilisateur.java
@@ -0,0 +1,29 @@
+
+public class Utilisateur {
+	
+	private String nomfaux;
+	private String prenom;
+	public Utilisateur() {
+		super();
+	}
+	public Utilisateur(String nom, String prenom) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	
+	
+
+}
